e912437403c66e676a3667ea0dc0a4ef6e0787bd
bryanvivancos <106763701+bryanvivancos@users.noreply.github.com>
2024-06-29 17:46:27 -0500

    Delete chapter_7 directory

--- chapter_7/multiples_of_ten.py
+++
@@ -1,6 +0,0 @@
-number = int(input("Enter a number to see if it's divisible by 10: "))
-
-if number % 10 == 0:
-    print("The number is divisible of 10")
-else:
-    print("The number is not divisible")
--- chapter_7/rental_car.py
+++
@@ -1,2 +0,0 @@
-car = input("Hi, what kind of rental car you would like? ")
-print(f"Let me see if I can find you a {car.title()}")
