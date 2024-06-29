e1f0faf3b007658813a81c9ddd3111f57e5411fc
bvivancosilva <bvivancosilva@gmail.com>
2024-06-29 22:42:49 +0000

    correccion

--- python_crash_course/buffet.py
+++
@@ -1,9 +0,0 @@
-foods = ("ice cream","rice","chicken","cheese","yogurt")
-print("Original Menu:")
-for food in foods:
-    print(food)
-
-foods = ("ice cream","rice","chicken","sandwich","pie")
-print("\nModified Menu")
-for food in foods:
-    print(food)
