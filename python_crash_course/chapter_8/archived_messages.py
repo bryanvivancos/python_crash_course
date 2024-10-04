c8046c11909e6c765e784983dcbfa1453962576f
bryan <bryanvivanco97@outlook.com>
2024-10-04 11:01:00 -0500

    cap 8 modified

--- python_crash_course/chapter_8/archived_messages.py
+++ python_crash_course/chapter_8/archived_messages.py
@@ -4,11 +4,14 @@ sents = []
 def send_messages(send_msg,sent_msg):
     while send_msg:
         text = send_msg.pop()
-        print(text)
         sent_msg.append(text)
 
-        
+def print_sents(sent_msg):
+    for sent in sent_msg:
+        print(sent)
+
 send_messages(send[:],sents)
+print_sents(sents)
 
 print("\nPrinting lists:")
 print(f'Enviados: {send}')
