b4bb538088400a23732d8507c53aa40e20781963
bryan <bryanvivanco97@outlook.com>
2024-10-11 01:29:22 -0500

    project 3 part 4

--- pizzeria_project/pizzas/urls.py
+++ pizzeria_project/pizzas/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'pizzas'
 urlpatterns = [
     #home page
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    #Pages that shows all models
+    path('pizzas',views.pizzas, name='pizzas'),
+    #Detail for a single pizza
+    path('pizzas/<int:pizza_id>/',views.pizza, name='pizza'),
 ]
