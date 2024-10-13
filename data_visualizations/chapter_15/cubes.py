279e5301eb7148564b7b8b65ebc23e64ccce36d0
bryan <bryanvivanco97@outlook.com>
2024-10-13 15:03:50 -0500

    project 2 part 1

---
+++ data_visualizations/chapter_15/cubes.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+nums = range(1,5001)
+cubic_nums = [x**3 for x in nums]
+
+print(plt.style.available) #styles availables
+plt.style.use('seaborn-v0_8') #use a plot style
+fig,ax = plt.subplots()
+ax.plot(nums,cubic_nums,linewidth=2)
+
+#set chat title and label axes.
+ax.set_title("Cubic Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Cubic of value", fontsize=14)
+
+#set size of tick labels.
+ax.tick_params(labelsize=8)
+
+#set the range for each axis.
+ax.axis([0,5100,0,140_000_000_000])
+
+#showing the plot
+plt.show()
